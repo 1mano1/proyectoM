3cab05791f3e4002e7152c284f90b940141f8711
1mano1 <orodriguez27@ucol.mx>
2024-02-29 18:50:17 -0600

    second commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// let valorx = document.getElementById('valorx');
+// let valorx0 = document.getElementById('valorx0');
+// let valorx1 = document.getElementById('valorx1');
+// let valorx2 = document.getElementById('valorx2');
+
+// let valorfx0 = document.getElementById('valorfx0');
+// let valorfx1 = document.getElementById('valorfx1');
+// let valorfx2 = document.getElementById('valorfx2');
+
+// let linear = document.getElementById('linear');
+// let resultado = document.getElementById('resultado');
+
+
+
+linear = valorfx0 + ((valorfx1 - valorfx2) / valorfx1 - valorfx0) * (valorx - valorx0);
+
+console.log(linear);
